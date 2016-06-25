cb80d9395a409274f64baf4a3d276013413860cf
gcmusic <gcmusic@gmail.com>
2016-06-25 19:49:04 +0300

    Client-side ratings

--- mod/settings.py
+++ mod/settings.py
@@ -121,4 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = r'C:\Users\gilad\Documents\MusicOnDemand\mod\mod\static'
+
 STATIC_URL = '/static/'
